80e79ff70c19cb36aadf6c328b8d7164499406d7
Caleb Skurdal <cskurdal@users.noreply.github.com>
2021-01-03 22:26:37 -0800

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,3 @@
+
+//Using SSD1306 128x64 pixel monochrome display withe Wemos D1 mini clone. CLK=>D1, SDA=>D2
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); //Tested with Arduino GraphicsTest
